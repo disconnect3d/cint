446fb0b73271178796878ba274b01c690cfacbc6
disconnect3d <dominik.b.czarnota@gmail.com>
2019-02-18 18:09:04 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cint',
+    description='cint - make ctypes great again',
+    url='https://github.com/disconnect3d/vmr',
+    author='disconnect3d',
+    version='0.1.0',
+    packages=find_packages(),
+    python_requires=['>=3.4', '>=2.7'],
+    install_requires=[],
+)
