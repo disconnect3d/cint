3eee482dafef6953b6816978fe50e1fce16fc2f4
disconnect3d <dominik.b.czarnota@gmail.com>
2019-02-28 18:47:36 +0100

    Fix setup!

--- setup.py
+++ setup.py
@@ -7,6 +7,5 @@ setup(
     author='disconnect3d',
     version='0.1.0',
     packages=find_packages(),
-    python_requires=['>=3.4', '>=2.7'],
-    install_requires=[],
+    python_requires='>=3.4,>=2.7',
 )
