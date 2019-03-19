db81c950bc717da11300231ceab39fb5c519ef31
disconnect3d <dominik.b.czarnota@gmail.com>
2019-03-19 01:29:12 +0100

    Pimp my setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,15 @@ setup(
     name='cint',
     description='cint - make ctypes great again',
     url='https://github.com/disconnect3d/cint',
+    version='1.0.0',
     author='disconnect3d',
-    version='0.1.0',
+    author_email='dominik.b.czarnota+cint@gmail.com',
     packages=find_packages(),
-    python_requires='>=3.4,>=2.7',
+    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+    ]
 )
